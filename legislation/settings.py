7268e1510ac4a6fe3d5d5511c0f7a9c73f1be89e
goose-life <32566441+goose-life@users.noreply.github.com>
2023-11-10 13:44:35 +0200

    add reader app settings: default language code

--- legislation/settings.py
+++ legislation/settings.py
@@ -115,6 +115,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+READER_APP_SETTINGS = {
+    'DEFAULT_LANGUAGE_CODE': 'eng',
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
