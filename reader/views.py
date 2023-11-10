5d7d3f977186058b519ff0431358ad0229a682c0
goose-life <32566441+goose-life@users.noreply.github.com>
2023-11-10 14:17:35 +0200

    views, templates, URLs for work listing and expression detail

--- reader/views.py
+++ reader/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Work, Expression
 
-# Create your views here.
+
+class WorkListView(ListView):
+    model = Work
+
+
+class ExpressionDetailView(DetailView):
+    model = Expression
+    slug_field = 'frbr_uri'
+    slug_url_kwarg = 'frbr_uri'
