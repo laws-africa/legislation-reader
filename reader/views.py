a6a0398c491edfea865a16c17c00d91afb965191
goose-life <32566441+goose-life@users.noreply.github.com>
2023-11-10 14:33:37 +0200

    add siblings to view, template for expression detail

--- reader/views.py
+++ reader/views.py
@@ -10,3 +10,9 @@ class ExpressionDetailView(DetailView):
     model = Expression
     slug_field = 'frbr_uri'
     slug_url_kwarg = 'frbr_uri'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get all the other expressions on this expression's work
+        context['siblings'] = self.object.work.expressions.exclude(frbr_uri=self.object.frbr_uri)
+        return context
